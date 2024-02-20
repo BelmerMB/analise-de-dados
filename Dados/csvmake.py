d2be851f51ca8644a22dda6243a136a501800119
Belmer <elmermetrasbrum@gmail.com>
2024-02-19 22:58:36 -0300

    Database

---
+++ Dados/csvmake.py
@@ -0,0 +1,9 @@
+from json import loads, dumps
+import pandas as pd
+
+df = pd.DataFrame(
+     [["a", "b"], ["c", "d"]],
+     index=["row 1", "row 2"],
+    columns=["col 1", "col 2"],
+)
+print(df)
