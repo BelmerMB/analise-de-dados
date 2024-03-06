2bdb615e359ef853475766d4555f2a7fc84fa360
Belmer <elmermetrasbrum@gmail.com>
2024-03-05 22:44:14 -0300

    novos dados

--- Dados/csvmake.py
+++
@@ -1,9 +0,0 @@
-from json import loads, dumps
-import pandas as pd
-
-df = pd.DataFrame(
-     [["a", "b"], ["c", "d"]],
-     index=["row 1", "row 2"],
-    columns=["col 1", "col 2"],
-)
-print(df)
