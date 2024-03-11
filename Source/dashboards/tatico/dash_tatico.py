48f1b9c39405f96ced2b4c114cfa6b46b3fd1776
Belmer <elmermetrasbrum@gmail.com>
2024-03-10 22:41:20 -0300

    Mudanca grafico

--- Source/dashboards/tatico/dash_tatico.py
+++ Source/dashboards/tatico/dash_tatico.py
@@ -3,6 +3,7 @@ import streamlit as st
 import numpy as np
 import matplotlib.pyplot as plt
 import plotly.express as px
+import time
 
 
 def salesPerCountry(df, country): #retorno somente duas colunas, perco informações de outras.
@@ -23,19 +24,49 @@ st.set_page_config(
 )
 
 st.write("# Welcome to dashboard! 👋")
+col1, col2 = st.columns(2)
+with col1:
+    st.header("Column 1")
+    st.write("Some data")
+    st.image("https://static.streamlit.io/examples/cat.jpg", width=128)
 
-# st.sidebar.success("In working.")
+with col2:
+    st.header("Column 2")
+    st.write("Some more data")
 
 st.markdown("""
 <style>
-body {
-background-color: blue;
+body
+{
+    background-color: blue;
 }
 </style>
 """, unsafe_allow_html=True
 )
 
 st.sidebar.success('select a demo')
+st.sidebar.success("In working.")
+# Just add it after st.sidebar:
+a = st.sidebar.radio('Choose:',[1,2])
+
+with st.chat_message("user"):
+    st.write("Hello 👋")
+    st.chat_input("Say something")
+    st.write('resposta')
+# st.toast('Mr Stay-Puft')
+
+bar = st.progress(50)
+time.sleep(3)
+bar.progress(100)
+with st.container():
+    st.write("This is inside a container")
+
+
+# st.button('Aperte aqui!', type='primary')
+# if st.button('say heelo'):
+#     st.write('putz')
+# else:
+#     st.write('saiu')
 
 
 
@@ -43,21 +74,57 @@ st.sidebar.success('select a demo')
 #----------------apresentação dos dados------------------
 x = st.slider('x', min_value=1, max_value=15)
 st.write(x, 'squared is', x*x)
-sales_country = df.groupby(['COUNTRY', 'CITY'])['SALES'].sum().reset_index()
+sales_country = df.groupby(['COUNTRY', 'YEAR_ID'])['SALES'].sum().reset_index()
 sales_USA = salesPerCountry(df, 'USA')
-sales_country.sort_values(by='SALES', inplace=True)
-fig = px.sunburst(df, path=['COUNTRY', 'CITY'], values='SALES', color='COUNTRY')
+sales_country.sort_values(by='YEAR_ID', inplace=True)
+
+country_iso_mapping = {
+    'USA': 'USA',
+    'France': 'FRA',
+    'Norway': 'NOR',
+    'Australia': 'AUS',
+    'Finland': 'FIN',
+    'Austria': 'AUT',
+    'UK': 'GBR',
+    'Spain': 'ESP',
+    'Sweden': 'SWE',
+    'Singapore': 'SGP',
+    'Canada': 'CAN',
+    'Japan': 'JPN',
+    'Italy': 'ITA',
+    'Denmark': 'DNK',
+    'Belgium': 'BEL',
+    'Philippines': 'PHL',
+    'Germany': 'DEU',
+    'Switzerland': 'CHE',
+    'Ireland': 'IRL'
+}
+
+# Create a scatter_geo plot using ISO codes
+fig = px.choropleth(sales_country, 
+                     locations=sales_country['COUNTRY'].map(country_iso_mapping),  # Map country names to ISO codes
+                    #  size='SALES',
+                    color_continuous_scale=px.colors.sequential.Plasma,
+                     color='COUNTRY',
+                     projection='natural earth',
+                     animation_frame="YEAR_ID",
+                     title='Scatter Geo Plot with Custom Country Names'
+                    )
 
 fig.update_layout(
-    height=1000,  # Set the desired height
-    width=1400,    # Set the desired width
     font=dict(size=22),
-    title=dict(
-        text="Vendas por paises",  # Set the title text
-        font=dict(
-            family="Arial",  # Set the font family for the title (optional)
-            size=18          # Set the font size for the title
-        )
-    )
-)
-st.plotly_chart(fig, use_container_width=True)
+    title_text='Vendas anual mundial')
+
+st.plotly_chart(fig, se_container_width=True)
+
+
+
+
+
+
+# fig = px.scatter_geo(sales_country, locations="COUNTRY", color="SALES",
+#                      hover_name="COUNTRY", projection="natural earth")
+# fig.show()
+# fig = px.sunburst(df, path=['COUNTRY', 'CITY'], values='SALES', color='COUNTRY')
+
+# st.plotly_chart(fig, use_container_width=True)
